94f4b030f84f92d5f48086ea638867411a750b43
Jazthedev <joelpepa2@gmail.com>
2023-07-20 21:18:50 +0200

    start project

---
+++ src/main/java/com/gridbetjavaa/gridbetjavaa/GridBetJavaaApplication.java
@@ -0,0 +1,13 @@
+package com.gridbetjavaa.gridbetjavaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridBetJavaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridBetJavaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gridbetjavaa/gridbetjavaa/GridBetJavaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gridbetjavaa.gridbetjavaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridBetJavaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
