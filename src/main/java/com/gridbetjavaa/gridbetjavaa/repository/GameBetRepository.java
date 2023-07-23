094afd50b222c4e8a65238438cea6c2094122a01
Jazthedev <joelpepa2@gmail.com>
2023-07-23 20:58:29 +0200

    add GameBet repository

---
+++ src/main/java/com/gridbetjavaa/gridbetjavaa/repository/GameBetRepository.java
@@ -0,0 +1,8 @@
+package com.gridbetjavaa.gridbetjavaa.repository;
+
+
+import com.gridbetjavaa.gridbetjavaa.model.GameBet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameBetRepository extends JpaRepository<GameBet,Long> {
+}
